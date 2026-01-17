4d892738f5f5ff383ce68f63f267ca9c0b49a9b9
Vinicius Mariani <vini.mariani.santos@gmail.com>
2026-01-17 19:39:20 -0300

    add config ts

--- backend/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const env = require('dotenv');
-env.config();
-
-const app = express();
-const port = Number(process.env.PORT) || 3000;
-
-app.get('/', (req, res) => res.send('<h1>Aplicação rodando em produção com sucesso!</h1>'));
-app.listen(port, () => console.log(`App rodando na porta ${port}`));
